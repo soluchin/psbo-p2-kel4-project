fd50076b2c359c641ab0d923be638a1a3009716f
TobiAlbertino <tobialbertino@gmail.com>
2021-06-07 18:59:00 +0700

    test id

--- prisma/migrations/20210602172532_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "firstName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL
-);
