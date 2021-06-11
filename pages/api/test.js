ba1a9c08a0d3f03061fae9173e4152f426dfe5c9
TobiAlbertino <tobialbertino@gmail.com>
2021-06-11 10:16:33 +0700

    test api

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,14 +1,30 @@
-import sqlite from "sqlite";
 import { PrismaClient, Prisma } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export default async function handler(req, res) {
-  // const db = await sqlite.open("../../prisma/my-db.db");
+export default async function addUserHandler(req, res) {
+  // const { userName, password } = req.body
 
   if (req.method === "POST") {
     // Process a POST request
+    try {
+      const { userName, password } = req.body
+      const result = await prisma.ddmin.create({
+        data: {
+          userName,
+          password
+        },
+      })
+      // res.json(result);
+      res.status(200).json({
+        status: 'Successfully registered!',
+        result
+      });
+    } catch (err) {
+      res.status(400).json({ message: 'Something went wrong' });
+    }
   } else {
     // Handle any other HTTP method
+    return res.status(405).json({ message: 'Method not allowed-SALAH WOY' });
   }
 }
